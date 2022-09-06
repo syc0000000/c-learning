e8cca0839a911cab45d69a0257e197730cbbdae1
syc0000000 <112924913+syc0000000@users.noreply.github.com>
2022-09-06 14:13:25 +0800

    Update 5.c

--- icoding/实验一/5.c
+++ icoding/实验一/5.c
@@ -1,21 +1,20 @@
-/*
+
 #include<stdio.h>
 int main() {
-	//³õÊ¼»¯
+	//Â³ÃµÃŠÂ¼Â»Â¯
 	int m1 = 0, s1 = 0, m2 = 0, s2 = 0, s = 0, m = 0;
 
-	//ÊäÈë
+	//ÃŠÃ¤ÃˆÃ«
 	printf("Enter two fractions separated by a plus sign:");
 	scanf_s("%d/%d+%d/%d", &s1, &m1, &s2, &m2);
 
-	//¼ÆËã
+	//Â¼Ã†Ã‹Ã£
 	s = s1 * m2 + s2 * m1;
 	m = m1 * m2;
 
-	//Êä³ö
+	//ÃŠÃ¤Â³Ã¶
 	printf("The sum is %d/%d", s, m);
 
 	return 0;
 
 }
-*/
